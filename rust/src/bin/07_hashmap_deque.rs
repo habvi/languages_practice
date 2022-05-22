1b492d8a627bf99cf6b8843046b649719e54c144
habvi <a2231250601h@yahoo.co.jp>
2022-05-23 00:30:37 +0900

    add: hashmap, result

--- rust/src/bin/07_hashmap_deque.rs
+++ rust/src/bin/07_hashmap_deque.rs
@@ -1,21 +1,49 @@
 use std::collections::HashMap;
 
 fn _hashmap() {
-    // make empty hashmap
-    let mut colors: HashMap<String, String> = HashMap::new();
+    {
+        let mut colors: HashMap<String, String> = HashMap::new();
 
-    colors.insert(String::from("Apple"), String::from("Red"));
-    colors.insert(String::from("Lemon"), String::from("Yellow"));
+        colors.insert(String::from("Apple"), String::from("Red"));
+        colors.insert(String::from("Lemon"), String::from("Yellow"));
 
-    let stuff: &str = "Lemon";
-    // return Option<&Value>. Wrapped by "Some()"
-    println!("{} : {:?}", stuff, colors.get(stuff));
+        let stuff: &str = "Lemon";
+        // return Option<&Value>. Wrapped by "Some()"
+        println!("{} : {:?}", stuff, colors.get(stuff));
 
-    let rm: &str = "Apple";
-    colors.remove(rm);
-    println!("removed {}", rm);
-    // if key not in keys, return None
-    println!("{:?}", colors.get(rm));
+        let rm: &str = "Apple";
+        colors.remove(rm);
+        println!("removed {}", rm);
+        // if key not in keys, return None
+        println!("{:?}", colors.get(rm));
+
+        colors.insert("Orange".to_string(), "Orange".to_string());
+        if colors.contains_key(&"Apple".to_string()) {
+            println!("unreachable");
+        }
+
+        // if not found -> panic
+        println!("{}", colors["Lemon"]);
+
+        for (fruit, color) in &colors {
+            println!("{fruit}: {color}");
+        }
+        println!("-----");
+    }
+    {
+        let mut mp: HashMap<&str, usize> = HashMap::from([("abc", 6), ("def", 80)]);
+        println!("{:?}", mp);
+
+        // insert only if it doesn't already exist
+        mp.entry("zzz").or_insert(30);
+        mp.entry("zzz").or_insert(50);
+        println!("{:?}", mp);
+
+        // update value
+        let value = mp.entry("z").or_insert(7);
+        *value += 1;
+        println!("{:?}", mp);
+    }
 }
 
 use std::collections::VecDeque;
--- rust/src/bin/13_error_Result.rs
+++ rust/src/bin/13_error_Result.rs
@@ -15,9 +15,30 @@ fn safe_division(divided: f64, divisor: f64) -> Result<f64, DivisionByZeroError>
     }
 }
 
-fn main() {
+fn run_1() {
     println!("{:?}", safe_division(9.0, 3.0));
     println!("{}", safe_division(12.0, 3.0).unwrap());
     println!("{:?}", safe_division(9.0, 0.0));
     println!("{:?}", safe_division(0.0, 5.0));
+}
+
+// ---------------------------------------
+// same func_1, func_2
+fn func_1() -> Result(Success, Failure) {
+    match operation_that_might_fail() {
+        Ok(success) => success,
+        Err(failure) => return Err(failure),
+    }
+}
+
+fn funct_2() -> Result(Success, Failure) {
+    operation_that_might_fail()?
+}
+
+
+// ---------------------------------------
+fn main() {
+    run_1();
+    println!("-----");
+
 }
