52e0a2e9971fcede9e379e74e79e75b885d0097d
habvi <a2231250601h@yahoo.co.jp>
2022-08-25 23:29:09 +0900

    add: divisors.rs

---
+++ rust/README.md
@@ -0,0 +1,1 @@
+https://docs.microsoft.com/ja-jp/learn/paths/rust-first-steps/
---
+++ rust_library/divisors.rs
@@ -0,0 +1,15 @@
+fn div_list(x: usize) -> Vec<usize> {
+    let mut div_l: Vec<usize> = Vec::new();
+    let mut div_r: Vec<usize> = Vec::new();
+    let mut i: usize = 1;
+    while i * i <= x {
+        if x % i == 0 {
+            div_l.push(i);
+            if i != x / i {
+                div_r.push(x / i);
+            }
+        }
+        i += 1;
+    }
+    div_l.iter().cloned().chain(div_r.iter().rev().cloned()).collect()
+}
