0b715ca6a18b39591eb8b4e61c7b821338169b1c
habvi <a2231250601h@yahoo.co.jp>
2022-12-26 23:49:10 +0900

    go: deque itr

--- go/deque.go
+++ go/deque.go
@@ -5,6 +5,20 @@ import (
 	"fmt"
 )
 
+func deque_print_all(deque *list.List) {
+	for e := deque.Front(); e != nil; e = e.Next() {
+		fmt.Print(e.Value, " ")
+	}
+	fmt.Println()
+}
+
+func deque_print_all_reverse(deque *list.List) {
+	for e := deque.Back(); e != nil; e = e.Prev() {
+		fmt.Print(e.Value, " ")
+	}
+	fmt.Println()
+}
+
 func PopFrontDeque(deque *list.List) interface{} {
 	x := deque.Front()
 	if x == nil {
@@ -26,8 +40,14 @@ func main() {
 	deque.PushBack(2)
 	deque.PushBack(5)
 	deque.PushBack(8)
+
+	deque_print_all(deque)
+	deque_print_all_reverse(deque)
+
 	fmt.Println(deque.Front().Value)
 	fmt.Println(PopFrontDeque(deque))
+
 	fmt.Println(deque.Back().Value)
 	fmt.Println(PopBackDeque(deque))
+
 }
